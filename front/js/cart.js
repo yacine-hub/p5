7cdb190e2c1418d4035f8353ccc0f3220647dde5
ben ouirane <benoui.yacine@gmail.com>
2022-03-22 14:07:33 +0100

    afficher les elements sur la page  cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,8 +2,7 @@ window.onload = function(){
     getCart();
 }
 
-let produitStorage = JSON.parse(localStorage.getItem("produits"));
-console.log(produitStorage);
+
 
 
 /* si le panier est vide = panier vide 
@@ -11,34 +10,129 @@ sinon afficher le ou les produits selectionner(nom du produit, image du produit,
 je fait la boucle, je creer les éléments
 */
 function getCart(){
+    let produitStorage = JSON.parse(localStorage.getItem("produits"));
+    console.log(produitStorage);
+    let totalPrice;
     if (produitStorage === null){
-    alert("votre panier est vide")
+        alert("votre panier est vide")
     } else {
-        for (produitStorage in localStorage){ // insert les éléments du fichier cart.html 
+        let totalPrice = 0;
+        for (let i in produitStorage){ // insert les éléments du fichier cart.html 
+            fetch ("http://localhost:3000/api/products/"+ produitStorage[i]._id)
+            .then(response => response.json())
+            .then(data => {
+                console.log(produitStorage[i]);
+                displayProduct(data, produitStorage[i].qty, produitStorage[i].color);
+                console.log(produitStorage[i].qty)
+                console.log(data.price)
+                // 
+                totalPrice += data.price * produitStorage[i].qty;;// accummuler les prix de chaque par la quantité
+                console.log(totalPrice);
+                
+            }) 
+        }
         
-            let article = document.createElement("article");
-        document.querySelector("#cart_item").appendChild(article);
-        article.setAttribute(produitStorage.productID);
-
-        let prodImg = document.createElement("img");
-        document.querySelector("#cart_item_img").appendChild(prodImg);
-        prodImg.setAttribute(produitStorage.imageUrl);
-
-        let prodColor = document.createElement("couleur");
-        prodColor.innerHTML = produitStorage.product-color;
-       
-        
-        let prodQuantity = document.createElement("quantité");
-        document.querySelector("#cart__item__content__settings__quantity").appendChild(prodQuantity);
-        prodQuantity.innerHTML = "quantité: ";
-
-
-    }
     }
     
-}    
+}
 
 
+function displayProduct(prod, product, qty, color){
+    
+    console.log(product);
+    console.log(color);
+    let totalPriceByProduct = product.price * qty;
+    console.log( totalPriceByProduct)
+    
+    /*let prodArticle = document.createElement("article");
+    //document.querySelector("#cart_items").appendChild(prodArticle);
+    prodArticle.className = "cart_item";
+    prodArticle.setAttribute('data-id',prod._id);
+    prodArticle.setAttribute('data-color',prod._id);
+    
+    
+    let prodImg = document.createElement("img");
+    prodArticle.appendChild(prodImg);
+    prodImg.className = "cart_item_img";
+    prodImg.src = prod.imageUrl;
+    prodImg.alt = prod.altTxt;*/
+
+    let prodArticle = document.createElement("article");
+    document.querySelector("#cart__items").appendChild(prodArticle);
+    prodArticle.className = "cart__item";
+    prodArticle.setAttribute('data-id',prod._id);
+
+    // Insertion de l'élément "div"
+    let prodDiv = document.createElement("div");
+    prodArticle.appendChild(prodDiv);
+    prodDiv.className = "cart__item__img";
+
+    // Insertion de l'image
+    let prodImg = document.createElement("img");
+    prodDiv.appendChild(prodImg);
+    prodImg.src = prod.imageUrl;
+    prodImg.alt = prod.altTxt;
+    
+    // Insertion de l'élément "div"
+    let prodItemContent = document.createElement("div");
+    prodArticle.appendChild(prodItemContent);
+    prodItemContent.className = "cart__item__content";
+
+    // Insertion de l'élément "div"
+    let prodItemContentTitlePrice = document.createElement("div");
+    prodItemContent.appendChild(prodItemContentTitlePrice);
+    prodItemContentTitlePrice.className = "cart__item__content__titlePrice";
+    
+    // Insertion du titre 
+    let prodTitle = document.createElement("h2");
+    prodItemContentTitlePrice.appendChild(prodTitle);
+    prodTitle.innerHTML = prod.name;
+
+    // Insertion de la couleur
+    let prodColor = document.createElement("p");
+    prodTitle.appendChild(prodColor);
+    prodColor.innerHTML = prod.colors;
+    prodColor.style.fontSize = "15px";
+    
+    let prodPrice = document.createElement("p");
+    prodItemContentTitlePrice.appendChild(prodPrice);
+    prodPrice.innerHTML = prod.price + "€";
+
+    let prodItemSetting = document.createElement("div");
+    prodItemContent.appendChild(prodItemSetting);
+    prodItemSetting.className = "cart__item__content__settings";
+
+    let prodItemQty= document.createElement("div");
+    prodItemSetting.appendChild(prodItemQty);
+    prodItemQty.className = "cart__item__content__settings__quantity";
+
+    let prodQuantity = document.createElement("p");
+    prodItemQty.appendChild(prodQuantity);
+    prodQuantity.innerHTML = "Quantité : " ;
+
+    let quantité = document.createElement("input");
+    prodQuantity.appendChild(quantité);
+    quantité.className ="itemQuantity";
+    quantité.setAttribute("type", "number");
+    quantité.setAttribute("name","itemQuantity");
+    quantité.setAttribute("min","1");
+    quantité.setAttribute("max","100");
+    
+    
+        // Utilisation de "append" afin d'ajouter les éléments attribuer dpuis le tableau
+    /*prodArticle.append(productImg, /*productName,productText );
+    prodArticle.append(productArticle);
+    document.getElementById("cart__item").append(productArticle)*/
+   
+    /*
+    let prodColor = document.createElement("couleur");
+    prodColor.innerHTML = produitStorage.product-color;
+
+
+    let prodQuantity = document.createElement("quantité");
+    document.querySelector("#cart__item__content__settings__quantity").appendChild(prodQuantity);
+    prodQuantity.innerHTML = "quantité: ";*/
+}
 
 /*<!--  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
                 <div class="cart__item__img">
